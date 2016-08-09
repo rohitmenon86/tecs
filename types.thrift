786f2bfa4fa2edab0bfd774baa2e567f0c21ec9c
R. Menon <rohitmenon86@users.noreply.github.com>
2016-08-09 19:54:53 +0200

    TECS Files

---
+++ types.thrift
@@ -0,0 +1,12 @@
+namespace java de.dfki.tecs.examples.helloworld
+namespace cpp de.dfki.tecs.examples.helloworld
+namespace csharp de.dfki.tecs.examples.helloworld
+namespace py de.dfki.tecs.examples.helloworld
+namespace rb de.dfki.tecs.examples.helloworld
+
+
+struct HelloWorldEvent {
+	1:required string message = "HelloWorld";
+}
+
+
